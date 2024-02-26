2089242940a6a5d1c5b44b168572d3697d060d81
pecho <1179852618@qq.com>
2024-02-26 11:09:37 +0800

    项目结构搭建完成

---
+++ gulimail-coupon/src/main/java/com/pecho/gulimail/coupon/GulimailCouponApplication.java
@@ -0,0 +1,13 @@
+package com.pecho.gulimail.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimailCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailCouponApplication.class, args);
+    }
+
+}
---
+++ gulimail-coupon/src/test/java/com/pecho/gulimail/coupon/GulimailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pecho.gulimail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
